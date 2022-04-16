35ec6a7d78b974f24a37a7fd8da3206901c06baa
younes najjar <younes.najjar.96@gmail.com>
2022-04-16 08:54:58 +0000

    Adding production enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: true,
+    demo: 'saas', // other possible options are creative and modern
+    GOOGLE_MAPS_API_KEY: 'AIzaSyDpgQMpcfx1QU-8SM-ljcgjG4xrYtIYby4',
+    max_menu_notifs: 7,
+    // base_url: 'http://192.168.101.38:5006',
+    base_url: 'http://localhost:3000',      // testing errors handelers
+    multi_user: false,
+    enable_push_notification: false,
     firebase: {
         apiKey: "AIzaSyAIt8iKuFb7mzcJ051Mli0zMGklvRrqGfk",
         authDomain: "theclub-e315e.firebaseapp.com",
